c1b4e871222643b7068e17a0ee099f8850047c53
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-04-15 20:26:50 +0700

    . (#7)
    Co-authored-by: duonghoclaptrnh24 <142007627+duonghoclaptrinh24@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import './App.css';
-import Header_JS from './components/header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer_js from './components/footer';
-import Homepage1 from './pages/homepage';
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header_JS></Header_JS>
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<Homepage1></Homepage1>}></Route>
-          </Routes>
-        </main>
-        <Footer_js></Footer_js>
-      </Router>
-    </div>
-  );
+  return <Layout/>;
 }
 
 export default App;
+
