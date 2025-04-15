5a70db4ec15553766e5facf24ed47bbc06536f1b
VIEhackersama <haidong09092005@gmail.com>
2025-04-15 15:39:47 +0700

    bản hoàn chỉnh?

--- src/components/img_card.js
+++ src/components/img_card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../style/img.css'
 function Img_card({sources, alt})
- {
+{
     return <img src={sources} alt={alt} className=' m-1 img-fluid p-1 rounded-3' />;
 }
 export default Img_card;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,9 +1,6 @@
-import Card_js from "../components/card";
-import monhoc from '../data/subject.json'
-import Img_card from "../components/img_card";
 import AboutUs from "../components/about";
 import VideoPage from "./Videopage";
-function Homepage() {
+function Homepage1() {
     return (
         <div>
             <AboutUs></AboutUs>
@@ -11,4 +8,4 @@ function Homepage() {
         </div>
     );
 }
-export default Homepage;
+export default Homepage1;
