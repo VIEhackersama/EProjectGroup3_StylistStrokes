4869ef3400f01cb4d3945095cbee7a04466513d7
huyle-uii <huyle4549@gmail.com>
2025-04-17 22:20:18 +0700

    sua lai video.jsjs

--- StylistStrokes/frontend/src/components/video.js
+++ StylistStrokes/frontend/src/components/video.js
@@ -2,7 +2,7 @@ function Video(props)
 {
     return(
         <div className="card mt-4 shadow card-hover-shadow" style={{width: 'auto'}}>
-            <img src={props.image} className="card-img-top mx-auto d-block " style={{width: '27rem',height:"27rem"}} alt="..."></img>
+            <img src={props.image} className="card-img-top mx-auto d-block img-fluid " style={{width: '25rem',height:"20rem"}} alt="..."></img>
             <div className="card-body">
                 <span className="card-title">{props.name}</span>
                 <p className="card-text">{props.des}</p>
