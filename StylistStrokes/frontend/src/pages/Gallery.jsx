1f7e652fa4d3371abde5eb4368921712512f38c1
DuyAnK6 <doduyan29102006@gmail.com>
2025-04-18 20:15:00 +0700

    Thêm content cho gallery(Cần thêm trang và css)

--- StylistStrokes/frontend/src/pages/Gallery.jsx
+++ StylistStrokes/frontend/src/pages/Gallery.jsx
@@ -5,27 +5,36 @@ import "../styles/font.css"
 import introArt from "../assets/images/gallery-art3.jpg"
 import introArt2 from "../assets/images/gallery-art2.jpg"
 
+import GalleryDisplay from "../components/Gallery_page/g_page_display";
+
 const Gallery = () => {
   return <>
-    <Container>
-        <Row>
-            <Col lg="6">
-                <div className="page_intro">
-                    <h1 className="intro1 tangerine-regular fw-medium">Welcome to the </h1>
-                    <h1 className="intro2 mea-culpa-regular fw-semibold">Gallery</h1>
-                    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab labore libero illum odit, amet incidunt quae deleniti explicabo obcaecati impedit saepe earum veritatis officia minus dignissimos nemo soluta cum nesciunt.</p>
-                </div>
-            </Col>
+    <section>
+        <Container>
+            <Row>
+                <Col lg="6">
+                    <div className="page_intro">
+                        <h1 className="intro1 tangerine-regular fw-medium">Welcome to the </h1>
+                        <h1 className="intro2 mea-culpa-regular fw-semibold">Gallery</h1>
+                        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab labore libero illum odit, amet incidunt quae deleniti explicabo obcaecati impedit saepe earum veritatis officia minus dignissimos nemo soluta cum nesciunt.</p>
+                    </div>
+                </Col>
+                
+                <Col lg="3">
+                    <img className="intro-img img-fluid" src={introArt}></img>
+                </Col>
+                <Col lg="3">
+                    <img className="intro-img img-fluid" src={introArt2}></img>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+    <section>
+        <div className="container-fluid p-0 bg-black">
+            <GalleryDisplay></GalleryDisplay>
+        </div>
             
-            <Col lg="3">
-                <img className="intro-img" src={introArt}></img>
-            </Col>
-            <Col lg="3">
-                <img className="intro-img" src={introArt2}></img>
-            </Col>
-            
-        </Row>
-    </Container>
+    </section>
   
   </>
 };
