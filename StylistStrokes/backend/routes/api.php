495ac6ba10028e3dee4e2d4dc4e8372b9c0f8480
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-04-18 23:47:15 +0700

    18/4/25, merge
    * Move to public, fix the footer
    
    * (đọc comment trong shop item pls) shop và gallery (#16)
    
    Co-authored-by: DuyAnK6 <doduyan29102006@gmail.com>
    
    * install needed lib
    
    * checking duong
    
    * iii
    
    * .
    
    ---------
    
    Co-authored-by: duonghoclaptrinh24 <142007627+duonghoclaptrinh24@users.noreply.github.com>
    
    * Add register
    
    * Register done
    
    * Add login (bh sẽ điều hướng ra home nếu thành công)
    
    ---------
    
    Co-authored-by: DuyAnK6 <doduyan29102006@gmail.com>
    Co-authored-by: duonghoclaptrinh24 <142007627+duonghoclaptrinh24@users.noreply.github.com>

--- StylistStrokes/backend/routes/api.php
+++ StylistStrokes/backend/routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\Api\UserController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('register',[AuthController::class, 'register']);
-Route::post('login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function() {
-    Route::get('users', [UserController::class, 'index']);
+    Route::get('/users', [UserController::class, 'index']);
 });
