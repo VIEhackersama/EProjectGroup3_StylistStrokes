fbdd511b551fa023f67a3d6bd78632a9cc7078b0
huyle-uii <huyle4549@gmail.com>
2025-04-22 20:44:31 +0700

    commit1 (#28)
    sua lai videoPage, them phan trang web learn

--- StylistStrokes/frontend/src/components/Layout/Layout.jsx
+++ StylistStrokes/frontend/src/components/Layout/Layout.jsx
@@ -9,7 +9,6 @@ export default function Layout () {
     <>
       <Header/>
       <Routers/>
-      <VideoPage/>
       <Footer></Footer>
     </>
   );
---
+++ StylistStrokes/frontend/src/components/learn.js
@@ -0,0 +1,15 @@
+function Learn(props)
+{
+    return(
+        <div className=" form-control card mt-4 shadow card-hover-shadow" style={{width: 'auto'}}>
+            <img src={props.image} className="card-img-top mx-auto d-block img-fluid " style={{width: '25rem',height:"19rem"}} alt="..."></img>
+            <div className="card-body">
+                <span className="card-title">{props.name}</span>
+                <p className="card-text">{props.des}</p>
+                <a href="#" className="btn btn-primary">Search</a>
+            </div>
+        </div>
+    );
+
+}
+export default Learn;
