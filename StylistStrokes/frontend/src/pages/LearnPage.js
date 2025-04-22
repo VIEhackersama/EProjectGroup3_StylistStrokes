74f8559896111dff117164f530fbca2754622e2d
huyle-uii <huyle4549@gmail.com>
2025-04-22 20:28:35 +0700

    commit2

--- StylistStrokes/frontend/src/pages/LearnPage.js
+++ StylistStrokes/frontend/src/pages/LearnPage.js
@@ -1,8 +1,10 @@
 import Learnlist from "../components/learnlist";
+
 function LearnPage() {
   return (
     <div>
-      <section className="bg-[src('/image/title.jpg')] bg-cover bg-center text-white text-center py-32">
+      {/* Section với ảnh nền */}
+      <section className="bg-[url('/image/title.jpg')] bg-cover bg-center text-white text-center py-32">
         <div className="bg-black bg-opacity-60 p-10 rounded-md inline-block">
           <h2 className="text-5xl font-bold mb-4">
             Master the Art of Calligraphy
@@ -12,17 +14,20 @@ function LearnPage() {
           </p>
           <a
             href="#courses"
-            class="bg-white text-black px-6 py-3 rounded-full font-semibold hover:bg-gray-200 transition"
+            className="bg-white text-black px-6 py-3 rounded-full font-semibold hover:bg-gray-200 transition"
           >
             Explore Courses
           </a>
         </div>
       </section>
+
+      {/* Section khóa học */}
       <section id="courses" className="max-w-6xl mx-auto py-20 px-4">
         <h3 className="text-4xl font-bold text-center mb-12">Our Courses</h3>
-        <Learnlist></Learnlist>
+        <Learnlist />
       </section>
     </div>
   );
 }
+
 export default LearnPage;
