b9eccc496e7d26555ad1a143d09a42902ad79320
duonghoclaptrinh24 <142007627+duonghoclaptrinh24@users.noreply.github.com>
2025-04-26 19:24:46 +0700

    sửa
    sửa+Thêm Code

--- StylistStrokes/frontend/src/components/Layout/Layout.jsx
+++ StylistStrokes/frontend/src/components/Layout/Layout.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import Header from './../Header/Header';
 import Routers from '../../router/Routers';
 import Footer from './../Footer/Footer';
-import VideoPage from "../../pages/Videopage";
+
 export default function Layout () {
   return ( 
     <>
       <Header/>
       <Routers/>
-      <Footer></Footer>
+      <Footer/>
+      
     </>
   );
 };
