912b2c877d99654208047af6b1bd7b94717487f8
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-04-27 22:34:16 +0700

    Thêm mũi tên cho highlights thêm css vài chỗ (#51)
    Co-authored-by: DuyAnK6 <doduyan29102006@gmail.com>

--- StylistStrokes/frontend/src/components/Gallery_page/highlight_display.js
+++ StylistStrokes/frontend/src/components/Gallery_page/highlight_display.js
@@ -2,8 +2,17 @@ import React, { useState, useEffect } from 'react';
 import arts from "../../assets/data/gallery.json";
 import GalHighlights from './highlights';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import "../../styles/font.css";
-import "./gallery.css"
+import "../../styles/gallery_page.css"
+
+// Chạy những cái này để hiện icon
+// npm i --save @fortawesome/fontawesome-svg-core
+// npm install --save @fortawesome/free-solid-svg-icons
+// npm install --save @fortawesome/react-fontawesome
+
 
 const GroupbyStyle=()=>{
     const styleMap = {};
@@ -42,17 +51,38 @@ const HighlightDisplay=()=>{
     const handleIndicatorClick = (index) => {
         setCurrentStyleIndex(index);
     };
+    const handlePrev = () => {
+        setCurrentStyleIndex((prevIndex) => 
+            prevIndex === 0 ? styleKeys.length - 1 : prevIndex - 1
+        );
+    };
+    
+    const handleNext = () => {
+        setCurrentStyleIndex((prevIndex) => 
+            prevIndex === styleKeys.length - 1 ? 0 : prevIndex + 1
+        );
+    };
     
 
     return<>
         <div className='row justify-content-center text-center '>
             <h1 className='region'>{currentRegion}</h1>
-            <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
-                {currentArts.slice(0, 3).map((art) => (
-                    <div className="col" key={art.id}>
-                        <GalHighlights art={art}/>
-                    </div>
-                ))}
+
+            <div className="row justify-content-around ">
+                <div className='col-1 justify-content-center d-flex flex-column '>
+                    <FontAwesomeIcon className='arrow-icon' onClick={handlePrev} icon={faChevronLeft}/>
+                </div>
+                <div className='col-9 row-cols-1 row-cols-md-3 g-2 justify-content-center d-flex flex-row'>
+                    {currentArts.slice(0, 3).map((art) => (
+                        <div className="col-4" key={art.id}>
+                            <GalHighlights art={art}/>
+                        </div>
+                    ))}
+                </div>
+                
+                <div className='col-1 justify-content-center d-flex flex-column '>
+                    <FontAwesomeIcon className='arrow-icon' onClick={handleNext} icon={faChevronRight}/>
+                </div>
             </div>
             {/* Indicators */}
             <div className="d-flex justify-content-center mt-3">
@@ -64,8 +94,8 @@ const HighlightDisplay=()=>{
                     style={{
                     width: '12px',
                     height: '12px',
-                    border: '1px solid gray',
-                    backgroundColor: index === currentStyleIndex ? 'white' : 'gray',
+                    border: '0',
+                    backgroundColor: index === currentStyleIndex ? 'goldenrod' : 'gold',
                     opacity: 0.8,
                     }}
                 />
--- StylistStrokes/frontend/src/components/Gallery_page/highlights.js
+++ StylistStrokes/frontend/src/components/Gallery_page/highlights.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/font.css'
 import "../../styles/gallery_page.css"
+import { Link } from 'react-router-dom';
 import testImg from "../../assets/images/Gallery/siah_masho.jpg"
 
 
@@ -25,20 +26,21 @@ const GalHighlights = ({art}) => {
     }
 
     return(
-
-        <div className="card border-0 shadow-none p-0 art-card">
-            <div className="card border-0 shadow-none p-0 art-card">
-                <img className='img-fluid card-img-top object-fit-cover w-100 rounded-0' src={require(`../../assets/images/Gallery/${art.image}`)} alt={art.name}
-                style={{
-                    height:"300px"
-                }}></img>
-                <div className="overlay position-absolute top-0 start-0 w-100 h-100"></div>
-                <div className="card-img-overlay align-self-end text-start" style={{zIndex:'2'}}>
-                    <h4 className={`fw-bold text-white ${getFont(art.style)} art-name`}>{art.name}</h4>
-                    <p className="text-white art-description m-0">{art.s_des}</p>
+        <Link to={`/gallery/${art.id}`} onClick={() => window.scrollTo(0, 0)} className='text-decoration-none'>
+            <div className="card border-0 shadow-none p-0 art-card mx-3">
+                <div className="card border-0 shadow-none p-0 art-card">
+                    <img className='img-fluid card-img-top object-fit-cover w-100 rounded-0' src={require(`../../assets/images/Gallery/${art.image}`)} alt={art.name}
+                    style={{
+                        height:"300px"
+                    }}></img>
+                    <div className="overlay position-absolute top-0 start-0 w-100 h-100"></div>
+                    <div className="card-img-overlay align-self-end text-start" style={{zIndex:'2'}}>
+                        <h4 className={`fw-bold text-white ${getFont(art.style)} art-name`}>{art.name}</h4>
+                        <p className="text-white art-description m-0">{art.s_des}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
 
     )
     
