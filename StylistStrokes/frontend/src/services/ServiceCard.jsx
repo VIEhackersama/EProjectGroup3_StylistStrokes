0daace9916c1ab2e781bf946b7dbec9cece4e7f9
duonghoclaptrinh24 <142007627+duonghoclaptrinh24@users.noreply.github.com>
2025-04-28 16:11:41 +0700

    sua+them

--- StylistStrokes/frontend/src/services/ServiceCard.jsx
+++ StylistStrokes/frontend/src/services/ServiceCard.jsx
@@ -3,15 +3,14 @@ import "./service-card.css";
 
 const ServiceCard = ({item}) => {
 
-const {imgUrl, title,desc} = item
-return ( 
-     <div className="service__item card">
-        <div className="service__img card-img">
+    const {imgUrl, title,desc} = item
+  return ( 
+     <div className="service__item">
+        <div className="service__img">
            <img src={imgUrl} alt="" />
         </div>
         <h5>{title}</h5>
-        <p>{desc}</p>   
-
+        <p>{desc}</p>
     </div>
   );
 }; 
