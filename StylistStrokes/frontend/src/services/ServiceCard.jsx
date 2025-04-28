e2821672b47586f6cdd920839db75061d9d8c12e
VIEhackersama <haidong09092005@gmail.com>
2025-04-28 22:44:02 +0700

    Sửa sticky header

--- StylistStrokes/frontend/src/services/ServiceCard.jsx
+++ StylistStrokes/frontend/src/services/ServiceCard.jsx
@@ -4,17 +4,6 @@ import "./service-card.css";
 const ServiceCard = ({ item }) => {
    const { imgUrl, title, desc } = item;
    return (
-      <div className="service__item card">
-         <div className="service__img card__img">
-            <img src={imgUrl} alt="" />
-         </div>
-         <h5>{title}</h5>
-         <p>{desc}</p>
-      </div>
-   );
-};
-    const {imgUrl, title,desc} = item
-  return ( 
      <div className="service__item">
         <div className="service__img">
            <img src={imgUrl} alt="" />
