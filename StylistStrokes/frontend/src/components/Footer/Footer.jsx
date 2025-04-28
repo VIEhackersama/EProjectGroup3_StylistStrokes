3346bc5328cafac7896e88d27325210009fd34f1
VIEhackersama <haidong09092005@gmail.com>
2025-04-28 11:52:31 +0700

    Sửa frontend cho register page, thêm gg btn

--- StylistStrokes/frontend/src/components/Footer/Footer.jsx
+++ StylistStrokes/frontend/src/components/Footer/Footer.jsx
@@ -4,15 +4,15 @@ import Footer_2 from "./../Footer/Footer2";
 export default function Footer() {
   return (
     <div>
-      <footer
+      <div 
         style={{ backgroundColor: "#212f3c " }}
-        className="text-white py-5"
+        className="text-white py-5 footer"
       >
         <div className="container">
           <Footer_1></Footer_1>
           <Footer_2></Footer_2>
         </div>
-      </footer>
+      </div>
     </div>
   )
 };
