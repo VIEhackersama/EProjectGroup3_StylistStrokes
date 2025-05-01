069db94b2673d5311d0c5b43551ec766341148a5
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-05-01 15:39:33 +0700

    Huy1 (#77)
    * commit1
    
    sua lai videoPage, them phan trang web learn
    
    * commit2
    
    * 1
    
    commit lai
    
    * 2
    
    sua lai learnpage
    
    * Update LearnPage.js
    
    commit 2
    
    * 2
    
    sua lai learnpage.js
    
    * 3
    
    sua lai card, videoPage, learnPage, va chinh lai mot so css
    
    * Update package-lock.json
    
    1
    
    * 4
    
    sua lai mot ti router cho dep. taoj lai trang rieng cho tung card trong videoPage
    
    * Update package-lock.json
    
    * 5
    
    sua lai phan Aboutpage
    
    * 5
    
    them trang của từng card khóa học rieng
    
    * 6
    
    sua lai giao dien o trang learn, trang about
    
    ---------
    
    Co-authored-by: huyle-uii <huyle4549@gmail.com>

--- StylistStrokes/frontend/src/pages/About.jsx
+++ StylistStrokes/frontend/src/pages/About.jsx
@@ -4,7 +4,7 @@ import AboutTitle from "../components/Home/abouttitle";
 export default function AboutPage()
 {
     return(
-        <div>
+        <div style={{ backgroundImage:"url(https://img.freepik.com/free-photo/canvas-background-with-paint-texture_91008-505.jpg?semt=ais_hybrid&w=740)"}}>
             <div className=" container py-5">
                 <AboutTitle></AboutTitle>
                 <AboutMiddle></AboutMiddle>
