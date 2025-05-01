1ef39f637a51a81f833ec0b871d30fbb15502ed5
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-05-01 15:40:46 +0700

    . (#78)
    * Hotfix (#72)
    
    * Sửa sticky header
    
    * Update login page
    
    * Update login
    
    * Nhap vao branch, ignore this commit(#67)
    
    * Dong update login (#65)
    
    * Sửa sticky header
    
    * Update login page
    
    * Update login
    
    * Huy1 (#66)
    
    * commit1
    
    sua lai videoPage, them phan trang web learn
    
    * commit2
    
    * 1
    
    commit lai
    
    * 2
    
    sua lai learnpage
    
    * Update LearnPage.js
    
    commit 2
    
    * 2
    
    sua lai learnpage.js
    
    * 3
    
    sua lai card, videoPage, learnPage, va chinh lai mot so css
    
    * Update package-lock.json
    
    1
    
    * 4
    
    sua lai mot ti router cho dep. taoj lai trang rieng cho tung card trong videoPage
    
    * Update package-lock.json
    
    * 5
    
    sua lai phan Aboutpage
    
    ---------
    
    Co-authored-by: D.Pham <74607020+VIEhackersama@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: huyle-uii <huyle4549@gmail.com>
    
    * Sửa tiếp header, login, register. Thêm bg vào home
    
    * Fix footer, thêm css cho header
    
    * Update Header.jsx
    
    ---------
    
    Co-authored-by: huyle-uii <huyle4549@gmail.com>
    
    * Huy1 (#73)
    
    * commit1
    
    sua lai videoPage, them phan trang web learn
    
    * commit2
    
    * 1
    
    commit lai
    
    * 2
    
    sua lai learnpage
    
    * Update LearnPage.js
    
    commit 2
    
    * 2
    
    sua lai learnpage.js
    
    * 3
    
    sua lai card, videoPage, learnPage, va chinh lai mot so css
    
    * Update package-lock.json
    
    1
    
    * 4
    
    sua lai mot ti router cho dep. taoj lai trang rieng cho tung card trong videoPage
    
    * Update package-lock.json
    
    * 5
    
    sua lai phan Aboutpage
    
    * 5
    
    them trang của từng card khóa học rieng
    
    ---------
    
    Co-authored-by: D.Pham <74607020+VIEhackersama@users.noreply.github.com>
    
    * Sửa layout gallery-detail (#76)
    
    Co-authored-by: DuyAnK6 <doduyan29102006@gmail.com>
    
    * Huy1 (#77)
    
    * commit1
    
    sua lai videoPage, them phan trang web learn
    
    * commit2
    
    * 1
    
    commit lai
    
    * 2
    
    sua lai learnpage
    
    * Update LearnPage.js
    
    commit 2
    
    * 2
    
    sua lai learnpage.js
    
    * 3
    
    sua lai card, videoPage, learnPage, va chinh lai mot so css
    
    * Update package-lock.json
    
    1
    
    * 4
    
    sua lai mot ti router cho dep. taoj lai trang rieng cho tung card trong videoPage
    
    * Update package-lock.json
    
    * 5
    
    sua lai phan Aboutpage
    
    * 5
    
    them trang của từng card khóa học rieng
    
    * 6
    
    sua lai giao dien o trang learn, trang about
    
    ---------
    
    Co-authored-by: huyle-uii <huyle4549@gmail.com>
    
    ---------
    
    Co-authored-by: huyle-uii <huyle4549@gmail.com>
    Co-authored-by: DuyAnK6 <doduyan29102006@gmail.com>

--- StylistStrokes/frontend/src/components/Gallery_page/highlights.js
+++ StylistStrokes/frontend/src/components/Gallery_page/highlights.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../styles/font.css'
+
 import "../../styles/gallery_page.css"
 import { Link } from 'react-router-dom';
 import testImg from "../../assets/images/Gallery/siah_masho.jpg"
@@ -27,19 +27,21 @@ const GalHighlights = ({art}) => {
 
     return(
         <Link to={`/gallery/${art.id}`} onClick={() => window.scrollTo(0, 0)} className='text-decoration-none'>
-            <div className="card border-0 shadow-none p-0 art-card mx-3">
-                <div className="card border-0 shadow-none p-0 art-card">
-                    <img className='img-fluid card-img-top object-fit-cover w-100 rounded-0' src={require(`../../assets/images/Gallery/${art.image}`)} alt={art.name}
-                    style={{
-                        height:"300px"
-                    }}></img>
+            
+                <div className="card border-0 shadow-none p-0 art-card" 
+                style={{
+                    height:"300px",
+                }}
+                >
+                    <img className='img-fluid card-img-top w-100 h-100 object-fit-cover rounded-0' src={require(`../../assets/images/Gallery/${art.image}`)} alt={art.name}
+                    ></img>
                     <div className="overlay position-absolute top-0 start-0 w-100 h-100"></div>
                     <div className="card-img-overlay align-self-end text-start" style={{zIndex:'2'}}>
                         <h4 className={`fw-bold text-white ${getFont(art.style)} art-name`}>{art.name}</h4>
-                        <p className="text-white art-description m-0">{art.s_des}</p>
+                        <p className="text-white art-description m-0 fs-6">{art.s_des}</p>
                     </div>
                 </div>
-            </div>
+            
         </Link>
 
     )
