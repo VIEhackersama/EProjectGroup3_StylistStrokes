2ee9201f040beb1fe8e55e2d6abfe5132b6c9eca
duonghoclaptrinh24 <142007627+duonghoclaptrinh24@users.noreply.github.com>
2025-05-04 00:22:49 +0700

    sua

--- StylistStrokes/frontend/src/pages/CaligraphyDetails.jsx
+++ StylistStrokes/frontend/src/pages/CaligraphyDetails.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import '../styles/caligraphy-details.css'
-import { Container, Row,Col, Form,ListGroup } from 'reactstrap'
-import {useParams} from 'react-router-dom'
+import React,{useRef, useState} from "react";
+import '../styles/caligraph-details.css'
+import { Container, Row,Col, Form, ListGroup } from 'reactstrap'
+import { useParams } from 'react-router-dom'
 import caligraphyData from '../assets/data/caligraphy'
-import calculateAvgRating from "../utils/avgRating";
+import calculateAvgRating from "./../utils/avgRating";
 import avatar from "../assets/images/avatar.jpg";
+import Booking from "../components/Booking/Booking";
 
 const CaligraphyDetails = () => {
-
-  const {id} = useParams()
+  const { id } = useParams();
+  const reviewMsRef = useRef('')
+  const [caligraphyRating, setCaligraphyRating]=useState(null)
 
   // this is an static data later we will call our API and load our data from database
   const caligraphy = caligraphyData.find(caligraphy=> caligraphy.id === id );
@@ -17,6 +19,18 @@ const CaligraphyDetails = () => {
   const {photo, title, desc, price, address, reviews, city, distance, maxGroupSize} = caligraphy;
 
   const {totalRating, avgRating} = calculateAvgRating(reviews);
+
+  // format date
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  
+  //submid
+  const submitHandler = e=>{
+    e.preventDefault()
+    const reviewText = reviewMsRef.current.value;
+
+    alert(`${reviewText},${caligraphyRating}`);
+
+  };
 
   return (
  <>
@@ -32,8 +46,8 @@ const CaligraphyDetails = () => {
 
                 <div className="d-flex align-items-center gap-5">
                   <span className="caligraphy__rating d-flex align-items-center gap-1">
-                     <i class="ri-star-fill" style={{color:"var(--secondary-color)"}}></i> {calculateAvgRating === 0 ? null : avgRating}
-                     {totalRating === 0 ? ( 'Not rated' ) : (
+                     <i class="ri-star-fill" style={{color:"var(--secondary-color)"}}></i> {avgRating === 0 ? null : avgRating}
+                     {totalRating === 0 ? (" Not rated" ) : (
                      <span>({reviews?.lenght})</span>
                      ) }
                  </span>
@@ -44,29 +58,49 @@ const CaligraphyDetails = () => {
                 </div>
 
                 <div className="caligraphy__extra-details">
-                  <span><i class="ri-map-pin-2-line"></i> {city}</span>
-                  <span><i class="ri-money-dollar-circle-line"></i> ${price} /
-                  per person</span>
-                  <span><i class="ri-group-line"></i> {maxGroupSize}</span>
+                  <span>
+                      <i class="ri-map-pin-2-line"></i> {city}
+                  </span>
+                  <span>
+                      <i class="ri-money-dollar-circle-line"></i> ${price} 
+                    /per person
+                  </span>
+                  <span>
+                      <i class="ri-map-pin-time-line"></i> ${distance}
+                    k/m
+                     </span>
+                  <span>
+                      <i class="ri-group-line"></i> {maxGroupSize} people
+                  </span>
                 </div>
                 <h5>Description</h5>
                 <p>{desc}</p>
               </div>
               {/* ======== caligraphy reviews section ======== */}
               <div className="caligraphy__reviews mt-4">
-                 <h4>Reviews ({reviews?.lenght} reviews)</h4>
+                 <h4>Reviews ({reviews?.length} reviews)</h4>
 
-                 <Form>
+                 <Form onSubmit={submitHandler}>
                   <div className="d-flex align-items-center gap-3 mb-4 rating__group">
-                        <span>1<i class="ri-star-s-fill"></i></span>
-                        <span>2<i class="ri-star-s-fill"></i></span>
-                        <span>3<i class="ri-star-s-fill"></i></span>
-                        <span>4<i class="ri-star-s-fill"></i></span>
-                        <span>5<i class="ri-star-s-fill"></i></span>
+                        <span onClick={()=> setCaligraphyRating(1)}>
+                          1<i class="ri-star-s-fill"></i>
+                        </span>
+                        <span onClick={()=> setCaligraphyRating(2)}>
+                          2<i class="ri-star-s-fill"></i>
+                        </span>
+                        <span onClick={()=> setCaligraphyRating(3)}>
+                          3<i class="ri-star-s-fill"></i>
+                        </span>
+                        <span onClick={()=> setCaligraphyRating(4)}>
+                          4<i class="ri-star-s-fill"></i>
+                        </span>
+                        <span onClick={()=> setCaligraphyRating(5)}>
+                          5<i class="ri-star-s-fill"></i>
+                        </span>
                   </div>
 
                   <div className="review__input">
-                      <input type="=text" placeholder="share your thoughts" />
+                      <input type="=text" ref={reviewMsRef} placeholder="share your thoughts"  required/>
                       <button className="btn primary__btn text-white" type="submit">
                         Submit
                       </button>
@@ -76,21 +110,36 @@ const CaligraphyDetails = () => {
                  <ListGroup className="user__reviews">
                     {reviews?.map(review => (
                         <div className="review__item">
-                           <img src={avatar} alt="" />
+                           <img src={review.avatar || avatar} alt="avatar" />
 
                            <div className="w-100">
-                             <div className="d-flex align-items-center justify-content-between">
-
+                             <div className="d-flex align-items-center 
+                             justify-content-between">
+                              <div>
+                                  <h5>muhbil</h5>
+                                  <p>
+                                    {new Date("01-18-2025").toLocaleDateString(
+                                      "en-US",
+                                      options
+                                    )}
+                                  </p>
+                              </div>
+                                <span className="d-flex align-items-center ">5<i class="ri-star-s-fill"></i></span>
                              </div>
+
+                             <h6>Amazing caligraphy</h6>
                            </div>
                         </div>
                       ))}
                  </ListGroup>
               </div>
-              {/* ======== caligraphy reviews end  =========== */}
-              
+              {/* ======== caligraphy reviews end  =========== */}             
           </div>
         </Col>
+         
+         <Col lg="4">
+            <Booking caligraphy={caligraphy} avgRating={avgRating}/> 
+         </Col>
       </Row>
     </Container>
   </section>
