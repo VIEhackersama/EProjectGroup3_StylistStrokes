55bcff963a3afd72231cec0d9b04014711e208f7
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-05-05 21:41:42 +0700

    Dong 13 (#107)
    * merge home
    
    * . (#95)
    
    * merge home (#92)
    
    * Create README.md
    
    * Update README.md
    
    * Huy8 (#94)
    
    * 1
    
    sua lai chut giao dien learnPage
    
    * 2
    
    sua lai giao dien cua từng card trang learn
    
    ---------
    
    Co-authored-by: huyle-uii <huyle4549@gmail.com>
    
    * Sửa tiếp UI cho contact
    
    * Add showcase page, sửa css cho tk huy
    
    * Update CaligraphyDetails.jsx
    
    * Update json
    
    * Backend for showcase, fix footer
    
    * Update favicon.ico
    
    * Xóa lorem ipsum cho about, footer
    
    * Update shop
    
    * Update Gallerysection.jsx
    
    * add motion cho showcase
    
    ---------
    
    Co-authored-by: huyle-uii <huyle4549@gmail.com>

--- StylistStrokes/frontend/src/pages/Interactive.jsx
+++ StylistStrokes/frontend/src/pages/Interactive.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import GallerySection from "../components/Interactive/Gallerysection";
 import FeedbackForm from "../components/Interactive/Galleryform";
-
-const InteractivePage = () => {
+import { motion } from "framer-motion";
+export default function InteractivePage() {
     return (
-        <div style={{
-            backgroundImage:"linear-gradient(to right, #ffa17f, #00223e)",
-
-        }}>
+        <div
+            style={{
+                backgroundImage: "linear-gradient(to right, #ffa17f, #00223e)"
+            }}
+        >
             <div className="position-relative vw-100 d-flex flex-column justify-content-center align-items-center">
                 <div
                     className="position-absolute top-0 start-0 w-100 h-100"
@@ -22,7 +23,7 @@ const InteractivePage = () => {
                 ></div>
                 <div className="position-relative text-center" style={{ zIndex: 2 }}>
                     <img
-                        style={{ filter: "invert(100%)", width: "50%" }} 
+                        style={{ filter: "invert(100%)", width: "50%" }}
                         className="p-3 img-fluid"
                         src="/image/logo1.png"
                         alt="Logo"
@@ -36,20 +37,19 @@ const InteractivePage = () => {
                     </a>
                 </div>
             </div>
-        <div className="container py-4">
+            <div className="container py-4">
                 <h1 className="mb-4 text-center text-white montserrat-nobro  ">The StylistStrokes Hall of Fame, but it is for everyone!</h1>
                 <h5 className="mb-4 text-center text-white montserrat-nobro ">Share your artistic creations with the world and let them shine.
-                   <br></br> Or simply your favorite calligraphy piece that you have made or seen.
-                   <br></br> Or basically anything, maybe jusy a mild improvement that you think is worth sharing with the world.
+                    <br></br> Or simply your favorite calligraphy piece that you have made or seen.
+                    <br></br> Or basically anything, maybe jusy a mild improvement that you think is worth sharing with the world.
 
                 </h5>
-            <GallerySection />
-            <hr className="my-5 text-white" />
+                <GallerySection />
+                <hr className="my-5 text-white" />
                 <h1 className="text-white mb-4 text-center montserrat-nobro ">It's your turn now! Don't let the world miss out</h1>
-            <FeedbackForm />
-        </div>
+                <FeedbackForm />
+            </div>
         </div>
     );
 };
 
-export default InteractivePage;
