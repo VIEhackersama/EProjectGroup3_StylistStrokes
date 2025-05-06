9cc2bf1f7038851281ce197071827839ae2ddde4
D.Pham <74607020+VIEhackersama@users.noreply.github.com>
2025-05-07 00:09:18 +0700

    Dong 15 (#115)
    * Add top bar, visited count
    
    * update btn
    
    * Dong 15 (#112) (#113)
    
    * Add top bar, visited count
    
    * update btn
    
    * Rename web

--- StylistStrokes/frontend/src/components/Header/Header.jsx
+++ StylistStrokes/frontend/src/components/Header/Header.jsx
@@ -67,11 +67,13 @@ const Header = () => {
   }, []);
   return (
     <div>
-      <div style={{ borderWidth: "10px", backgroundColor: "#f5a68c", color: "brown" ,borderColor:"brown"}} className="top-bar py-2">
+      <div style={{ borderWidth: "10px", backgroundColor: "#f5a68c", color: "brown", borderColor: "brown" }} className="top-bar py-2">
         <div className="container d-flex justify-content-between align-items-center">
           <div className="d-flex align-items-center">
             <FaMessage className="me-2" />
-            <span>Email: styliststrokes@customercare.com</span>
+            <span style={{ textDecoration: "underline" }}>Email: styliststrokes@customercare.com</span>
+            <FaPhone className="mx-3 me-2"></FaPhone>
+            <span>Phone: (+84) 234 567 890</span>
           </div>
           <div className="social-links">
             <label>Visited count: {visitCount}</label>
